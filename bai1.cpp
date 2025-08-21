411d1d24cf24a8cb0af1d3555a472739d8baf8fa
hdynn <danh8519@gmail.com>
2025-08-21 10:25:46 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=6,b=2;
+    cout<<"a+b="<<a+b<<endl;
+    cout<<"a-b="<<a-b<<endl;
+    cout<<"a*b="<<a*b<<endl;
+    cout<<"a/b="<<a/b<<endl;
+
+}
